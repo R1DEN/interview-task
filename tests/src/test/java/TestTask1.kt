40af8f175a09608e505c2f33a7977a2135b8537f
RiDEN <dredo.97@gmail.com>
2021-12-10 21:47:18 +0200

    added allure reporting

--- tests/src/test/java/TestTask1.kt
+++ tests/src/test/java/TestTask1.kt
@@ -1,17 +1,11 @@
-import com.google.gson.Gson
 import io.cucumber.junit.Cucumber
 import io.cucumber.junit.CucumberOptions
-import md.riden.interviewtask.common.logger
-import md.riden.interviewtask.api.PhotosAPI
-import md.riden.interviewtask.models.Photo
-import org.junit.Assert
-import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(Cucumber::class)
 @CucumberOptions(
     features = ["classpath:features"],
-    plugin = ["pretty", "json:target/cucumber.json"],
+    plugin = ["pretty", "json:target/cucumber.json", "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm"],
     glue = ["md.riden.interviewtask.context", "md.riden.interviewtask.stepDefinitions"],
 )
 class TestTask1
