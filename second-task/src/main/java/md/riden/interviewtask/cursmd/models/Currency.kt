ea4d74a749b3da7087f3824b5c1d87b6bfc26ce2
RiDEN <dredo.97@gmail.com>
2021-12-12 17:40:51 +0200

    fixed toString for Currency

--- second-task/src/main/java/md/riden/interviewtask/cursmd/models/Currency.kt
+++ second-task/src/main/java/md/riden/interviewtask/cursmd/models/Currency.kt
@@ -3,6 +3,6 @@ package md.riden.interviewtask.cursmd.models
 class Currency(val name: String, val rate: Rate) {
 
     override fun toString(): String {
-        return "$name    $rate$"
+        return "$name    $rate"
     }
 }
